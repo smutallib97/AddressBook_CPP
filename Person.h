645dcf0aeec93d283ae259244c129e1a92c6e71b
smutallib97 <smutallib97@gmail.com>
2022-12-13 12:30:30 +0530

    Added: Create a person and display details

---
+++ Person.h
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+private:
+    string firstName;
+    string lastName;
+    string state;
+    string city;
+    string zip;
+    string phoneNumber;
+
+public:
+    string getFirstName() {
+        return firstName;
+    }
+
+    void setFirstName(string firstName) {
+        this -> firstName = firstName;
+    }
+
+    string getLastName() {
+        return lastName;
+    }
+
+    void setLastName(string lastName) {
+        this -> lastName = lastName;
+    }
+
+    string getState() {
+        return state;
+    }
+
+    void setState(string state) {
+        this -> state = state;
+    }
+
+    string getCity() {
+        return city;
+    }
+
+    void setCity(string city) {
+        this -> city = city;
+    }
+
+    string getZip() {
+        return zip;
+    }
+
+    void setZip(string zip) {
+        this -> zip = zip;
+    }
+
+    string getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    void setPhoneNumber(string phoneNumber) {
+        this -> phoneNumber=phoneNumber;
+    }
+};
